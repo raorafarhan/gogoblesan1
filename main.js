5f8eee41cfec02ef87218a679cfeb42f3ad40f50
raorafarhan <raorafarhan@gmail.com>
2023-06-08 12:23:14 +0700

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+function ubahLatar(){
+  const target = document.getElementById('targetObject');
+  const input = document.getElementById('inputHexa');
+
+  target.style.backgroundColor = input.value;
+  
+}
